ea253792e3cbde57b3e0363bc896b0dabe694bee
carlospuigserver <cpuiggut@myuax.com>
2021-12-14 11:17:34 +0100

    hago el tablero e indico en que casos las fichas no se puedan mover

--- Ajedrez.py
+++ Ajedrez.py
@@ -49,3 +49,49 @@ def alteración(f, c):
         f= f- 1
     return f
     
+while True:
+    tablero =  [
+    [' ', ' ', ' '], 
+    [' ', ' ', ' '],
+    [' ', ' ', ' '], 
+    ]
+
+    x = randint(0,2)
+    y = randint(0,2)
+    z = randint(0,2)
+    a = randint(0,2)
+    b = randint(0,2)
+    c = randint(0,2)
+
+    while x == a:
+        a = randint(0,2)
+    while y == b:
+        b = randint(0,2)
+    while z == c:
+        c = randint(0,2)
+
+    #posicionpiezas
+    (tablero[x])[0] = chr(0x2656)
+    (tablero[y])[1] = chr(0x2656)
+    (tablero[z])[2] = chr(0x2656)
+    (tablero[a])[0] = chr(0x265C)
+    (tablero[b])[1] = chr(0x265C)
+    (tablero[c])[2] = chr(0x265C)
+
+    impr_tablero(tablero)
+
+    errorx = encerrada(x, 0)
+    errory = encerrada(y, 1)
+    errorz = encerrada(z, 2)
+    errora = encerrada(a, 0)
+    errorb = encerrada(b, 1)
+    errorc = encerrada(c, 2)
+
+    if errorx == True and errory == True and errorz == True:
+        print("El jugador blanco no se puede mover, volvemos a crear el tablero")
+        pass
+    elif errora == True and errorb == True and errorc == True:
+        print("El jugador negro no se puede mover, volvemos a crear el tablero")
+        pass
+    else:
+        break
