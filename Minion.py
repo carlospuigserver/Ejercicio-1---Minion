4a80aea527428571b4485bc0c4f4abe26cf75fd0
carlospuigserver <cpuiggut@myuax.com>
2021-12-14 10:45:52 +0100

    Impongo condiciones para el ganador

--- Minion.py
+++ Minion.py
@@ -4,3 +4,18 @@ Vocales=['AEIOU']
 l=len(palabra) #longitud de la palabra
 consonantes=0
 vocales=0
+
+
+
+
+
+
+
+
+if consonantes<vocales:
+    print("El ganador es Kevin con ",vocales,"puntos")
+if consonantes>vocales:
+    print("El ganador es Stuart con ",consonantes,"puntos")
+if consonantes==vocales:
+    ("La partida ha sido empatada a",consonantes,"puntos")
+print(" El número total de puntos obtenidos entre Kevin y Stuart es de ",consonantes+vocales,"puntos")
